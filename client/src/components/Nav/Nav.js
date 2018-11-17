98afd1f5a5936534253d8088a8440bad9ae6fc67
jyanglau88 <jyanglau88@gmail.com>
2018-11-16 23:54:11 -0800

    updates & fixes

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,23 +2,15 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 const Nav = () =>
-  <nav className="navbar navbar-inverse bg-inverse navbar-top">
+  <nav className="jumbotron main-bg-color">
     <div className="container-fluid">
-      <div className="navbar-header">
-        <button type="button" className="#">
-          <span className="sr-only">Menu</span>
-          <span className="icon-bar" /> <span className="icon-bar" />
-          <span className="icon-bar" />
-        </button>
-        <Link to="/" className="navbar-brand">
-          New York Times React Search
-        </Link>
-      </div>
-      <div className="#">
-        <ul className="nav">
-          <li><Link to="/search">Search</Link></li>
-          <li><Link to="/saved">Saved</Link></li>
-        </ul>
+    <h5 className="display-4">New York Times React Search</h5>
+      <div className="float-right">
+      <button className="btn btn-warning mr-3"> <Link to="/" >
+          Home
+        </Link></button>
+          <button className="btn btn-warning mr-3"><Link to="/search">Search</Link></button>
+          <button className="btn btn-warning"><Link to="/saved">Saved</Link></button>
       </div>
     </div>
   </nav>;
