20937fbd4106b3f956608db38707fca1309ee35c
jyanglau88 <jyanglau88@gmail.com>
2018-11-17 00:04:10 -0800

    updates

--- client/src/components/ArticleWell/Button/Button.js
+++ client/src/components/ArticleWell/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const button = (props) => (
-  <button className="btn btn-primary" onClick={props.clicked}>{props.title}</button>
+  <button className="btn btn-warning mr-3" onClick={props.clicked}>{props.title}</button>
 );
 
 export default button;
